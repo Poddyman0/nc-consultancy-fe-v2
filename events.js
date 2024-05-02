1fef14a9eecfc7464cb2c71575b3337d05476fd3
Poddyman0 <127142059+Poddyman0@users.noreply.github.com>
2024-05-02 18:14:18 +0100

    google callendar integration with events

--- events.js
+++ events.js
@@ -1,12 +1,10 @@
-
 document.addEventListener('DOMContentLoaded', function() {
     loadEvents()
-
 })
-
+/*
 let eventIDToView = ""
 
-export function handleEventButtonClick(event) {
+function handleEventButtonClick(event) {
     if (event) {
         eventIDToView = `${event.target.value}`;
         console.log("has event", eventIDToView);
@@ -20,15 +18,92 @@ export function handleEventButtonClick(event) {
     }
 
 }
+*/
 
-let eventsDisplay = []
+//let eventsDisplay = []
 
+//all returned events
 let eventsArray = []
+
+
+//individuals events
+let eventCallendar = []
+
+
+    /* exported gapiLoaded */
+  /* exported gisLoaded */
+  /* exported handleAuthClick */
+  /* exported handleSignoutClick */
+
+  // TODO(developer): Set to client ID and API key from the Developer Console
+  const CLIENT_ID = '1089595369803-i8rqgq0ql4tnhl8aoh0h7e1729stj569.apps.googleusercontent.com';
+  const API_KEY = 'AIzaSyAb15ND20iMQTKoAAyQWLKO4elU2ybdqOQ';
+
+  // Discovery doc URL for APIs used by the quickstart
+  const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
+
+  // Authorization scopes required by the API; multiple scopes can be
+  // included, separated by spaces.
+  const SCOPES = 'https://www.googleapis.com/auth/calendar';
+
+  let tokenClient;
+  let gapiInited = false;
+  let gisInited = false;
+
+  /**
+   * Callback after api.js is loaded.
+   */
+  function gapiLoaded() {
+    gapi.load('client', initializeGapiClient);
+  }
+
+  /**
+   * Callback after the API client is loaded. Loads the
+   * discovery doc to initialize the API.
+   */
+  async function initializeGapiClient() {
+    await gapi.client.init({
+      apiKey: API_KEY,
+      discoveryDocs: [DISCOVERY_DOC],
+    });
+    gapiInited = true;
+    //maybeEnableButtons();
+  }
+
+  /**
+   * Callback after Google Identity Services are loaded.
+   */
+  function gisLoaded() {
+    tokenClient = google.accounts.oauth2.initTokenClient({
+      client_id: CLIENT_ID,
+      scope: SCOPES,
+      callback: '', // defined later
+    });
+    gisInited = true;
+    //maybeEnableButtons();
+  }
+
+var gapiScript = document.createElement('script');
+gapiScript.src = 'https://apis.google.com/js/api.js';
+gapiScript.async = true;
+gapiScript.defer = true;
+gapiScript.onload = gapiLoaded;
+
+var gisScript = document.createElement('script');
+gisScript.src = 'https://accounts.google.com/gsi/client';
+gisScript.async = true;
+gisScript.defer = true;
+gisScript.onload = gisLoaded;
+
+// Append script elements to the document's head
+document.head.appendChild(gapiScript);
+document.head.appendChild(gisScript);
+
+
 
 function loadEvents() {
     const eventDisplay = document.querySelector('.card-container')
-    fetchEvents ()
-    function fetchEvents () {
+
             fetch(`https://nc-events-platform-be-v2-production.up.railway.app/platform/events/get`, {
                 method: 'GET',
             })
@@ -36,7 +111,9 @@ function loadEvents() {
                 return response.json();
             })
             .then(function(response) {
+                console.log(response)
                 response.forEach(aEvent => {
+                    console.log(aEvent)
 
                     eventsArray.push(aEvent)
                     let aEventToDisplay = document.createElement('div')
@@ -44,49 +121,61 @@ function loadEvents() {
                     aEventToDisplay.id = `event-card-${aEvent._id}`
                     aEventToDisplay.innerHTML = `
                         <div class="card-body">
+                        <img class="card-img-top" src="${aEvent.eventPicture}" alt="Event Picture">
                             <div class="card-title-icon-container">
-                                <h5 class="card-title">${aEvent.eventName}</h5>
+                                <div class="card-title event-card-title">${aEvent.eventName}</div>
                             </div>
-                        <p class="card-text">Description: ${aEvent.eventDescription}</p>
-                        <p class="card-text">Start Date: ${aEvent.eventStartDate}</p>
-                        <p class="card-text">Start Time: ${aEvent.eventStartTime}</p>
-                        <p class="card-text">End Date: ${aEvent.eventEndDate}</p>
-                        <p class="card-text">End Time: ${aEvent.eventEndTime}</p>
-                        <p class="card-text">Location: ${aEvent.eventBuildingNumber}, ${aEvent.eventStreetName}, ${aEvent.eventCity}, ${aEvent.eventCounty}, ${aEvent.eventCountry}, ${aEvent.eventPostCode}</p>
-                        <p class="card-text">Pricing: ${aEvent.eventPricing}</p>
-                        <p class="card-text">Price: £${aEvent.eventTicketPrice}</p>
-                        <p class="card-text">Amount Of Tickets Left: ${aEvent.eventTicketAmount}</p>
+                        <ul>
+                        <li class="card-text"><strong>Description: </strong>${aEvent.eventDescription}</li>
+                        <li class="card-text"><strong>Start Date: </strong>${aEvent.eventStartDate}</li>
+                        <li class="card-text"><strong>Start Time: </strong>${aEvent.eventStartTime}</li>
+                        <li class="card-text"><strong>End Date: </strong>${aEvent.eventEndDate}</li>
+                        <li class="card-text"><strong>End Time: </strong>>${aEvent.eventEndTime}</li>
+                        <li class="card-text"><strong>Location: </strong>${aEvent.eventBuildingNumber}, ${aEvent.eventStreetName}, ${aEvent.eventCity}, ${aEvent.eventCounty}, ${aEvent.eventCountry}, ${aEvent.eventPostCode}</li>
+                        <li class="card-text"><strong>Pricing: </strong>${aEvent.eventPricing}</li>
+                        <li class="card-text"><strong>Price: </strong>£${aEvent.eventTicketPrice}</li>
+                        <li class="card-text"><strong>Tickets Left: </strong>${aEvent.eventTicketAmount}</li>
+                        </ul>
                         <form class="card-text">
-                                    <label for="add-to-cart-amount">Amount of tickets you want to purchase:</label>
-                                    <input type="number" class="form-control" id="add-to-cart-amount" placeholder="Enter amount of tickets you would like to buy." required>
-                                    <div class="invalid-feedback add-to-cart-amount-feedback"></div>
-                                    <button class="btn btn-warning add-to-cart-button" value="${aEvent}">Add To Event To Callendar And Cart</button>
+                                    <label for="add-to-cart-amount-${aEvent._id}"><strong>Amount of tickets you want to add to your callendar:</strong></label>
+                                    <input type="number" class="form-control" id="add-to-cart-amount-${aEvent._id}" placeholder="Enter amount of tickets you would like to buy." required>
+                                    <div class="invalid-feedback" id="add-to-cart-amount-feedback-${aEvent._id}"></div>
                         </form>
-                        <button class="btn btn-danger" id="delete-event">Delete Event</button>
-                        <p id="added-to-cart-feedback"></p>
+                        <button class="btn btn-warning card-btn" value="${aEvent}" id="add-to-cart-button-${aEvent._id}">Add To Event To Callendar And Cart</button>
+                        <button class="btn btn-danger card-btn" id="delete-event">Delete Event</button>
+                       
+                        <button class="btn btn-primary" style="width: 100%" id="authorize_button-${aEvent._id}" onclick="handleAuthClick(${aEvent})">Add Event To Google Callendar</button>
+                        <button class="btn btn-primary" style="width: 100%" id="signout_button-${aEvent._id}" onclick="handleSignoutClick()">Sign Out of Google Callendar</button>
+                        <a class="btn btn-primary" style="width: 100%" id="content-${aEvent._id}">Link To Event Created On Google Callendar</a>
+                        
+                        <ul>
+                        <li id="added-to-cart-feedback"></li>
+                        </ul>
                         </div>
                     `
-                    //                         <button class="btn btn-warning" value="${aEvent._id}" id="btn-sign-up-to-event">Sign Up To Event</button>
-
-                    //  <img class="card-img-top" src="${event.eventPicture}" alt="Event Picture">
-                    // button to view event: <button type="button" value="${event._id}" id="btn-event-card-${event._id}" class="btn btn-primary btn-events-info">Event Info</button>
+                    eventDisplay.appendChild(aEventToDisplay)                    
+                    
+                    //<button class="btn btn-warning" value="${aEvent._id}" id="btn-sign-up-to-event">Sign Up To Event</button> 
                     // delete event in event.js
-                    eventDisplay.appendChild(aEventToDisplay)
+                    let addToCartBtn = document.querySelector(`#add-to-cart-button-${aEvent._id}`)
+                    addToCartBtn.addEventListener('click', function (event) {
+                        event.preventDefault 
+                        //let eventObj = event.target.value;
+                        //let eventObj = event.target.value;
 
-                    // button to view event: document.getElementById(`btn-event-card-${event._id}`).addEventListener('click', handleEventButtonClick);
-                    
-                    const ticketAmountPurchase = document.querySelector('#add-to-cart-amount')
-                    const addToCartAmountFeedback = document.querySelector('.add-to-cart-amount-feedback')
-                    const addedToCartFeedback = document.querySelector('#added-to-cart-feedback')
-                    //const updateEventButton = document.querySelector('#btn-update-event-internal')
-                    //updateEventButtonExport = updateEventButton
-                    //const signUpToEventButton = document.querySelector('#btn-sign-up-to-event')
-                    //signUpToEventButtonExport = signUpToEventButton
-                    addedToCartFeedback.innerHTML = ""
-                    
-                    document.querySelector(`#add-to-cart-button`).addEventListener('click', function (event) {
-                        event.preventDefault()
-                        let eventObj = event.target.value;
+                        console.log("aEvent", aEvent)
+                        //console.log("eventOBJ", eventObj)
+
+                        // button to view event: document.getElementById(`btn-event-card-${event._id}`).addEventListener('click', handleEventButtonClick);  
+                        const ticketAmountPurchase = document.querySelector(`#add-to-cart-amount-${aEvent._id}`)
+                        const addToCartAmountFeedback = document.querySelector(`#add-to-cart-amount-feedback-${aEvent._id}`)
+                        const addedToCartFeedback = document.querySelector('#added-to-cart-feedback')
+                        //const updateEventButton = document.querySelector('#btn-update-event-internal')
+                        //updateEventButtonExport = updateEventButton
+                        //const signUpToEventButton = document.querySelector('#btn-sign-up-to-event')
+                        //signUpToEventButtonExport = signUpToEventButton
+                        addedToCartFeedback.innerHTML = ""
+                        console.log("eventOBJ", aEvent._id)
                         if (ticketAmountPurchase.value.length === 0 || ticketAmountPurchase.value > aEvent.eventTicketAmount) {
                             ticketAmountPurchase.className = "form-control is-invalid"
                             addToCartAmountFeedback.innerHTML = "Amount of tickets purchased field must not be empty or be greater than the amount of tickets available"
@@ -94,17 +183,114 @@ function loadEvents() {
                             ticketAmountPurchase.className = "form-control"
                             addToCartAmountFeedback.innerHTML = ""
                             addedToCartFeedback.innerHTML = "Event successfully added to cart"
-                            eventCallendar.push(eventObj)
+                            eventCallendar.push(aEvent)
+                            console.log("eventCallendar", eventCallendar)
                         }
-                    });
-                    
-            })
-            .catch(function(err) {
-                console.log("Error: ", err);
+                    })
+                    const authorizeButton = document.getElementById(`authorize_button-${aEvent._id}`)
+                    authorizeButton.style.visibility = 'hidden';
+                    const signOutButton = document.getElementById(`signout_button-${aEvent._id}`)
+                    signOutButton.style.visibility = 'hidden';
+                    const eventLinkButton = document.getElementById(`content-${aEvent._id}`)
+                    eventLinkButton.style.visibility = 'hidden';
+                      /**
+                     * Enables user interaction after all libraries are loaded.
+                     */
+                    //function maybeEnableButtons() {
+                        if (gapiInited && gisInited) {
+                            authorizeButton.style.visibility = 'visible';
+                        }
+                   // }
+
+                    /**
+                     *  Sign in the user upon button click.
+                     */
+                    function handleAuthClick(eventToAddToCallendar) {
+                        tokenClient.callback = async (resp) => {
+                        if (resp.error !== undefined) {
+                            throw (resp);
+                        }
+                        signOutButton.style.visibility = 'visible';
+                        eventLinkButton.style.visibility = 'visible';
+                        await createEventGoogleCallendar(eventToAddToCallendar)
+                        };
+
+                        if (gapi.client.getToken() === null) {
+                        // Prompt the user to select a Google Account and ask for consent to share their data
+                        // when establishing a new session.
+                        tokenClient.requestAccessToken({prompt: 'consent'});
+                        } else {
+                        // Skip display of account chooser and consent dialog for an existing session.
+                        tokenClient.requestAccessToken({prompt: ''});
+                        }
+                    }
+
+                    /**
+                     *  Sign out the user upon button click.
+                     */
+                    function handleSignoutClick() {
+                        const token = gapi.client.getToken();
+                        if (token !== null) {
+                        google.accounts.oauth2.revoke(token.access_token);
+                        gapi.client.setToken('');
+                        eventLinkButton.style.visibility = 'hidden';
+                        authorizeButton.innerText = 'Add Event To Google Callendar';
+                        signOutButton.style.visibility = 'hidden';
+                        }
+                    }
+                      /*
+                        * Print the summary and start datetime/date of the next ten events in
+                        * the authorized user's calendar. If no events are found an
+                        * appropriate message is printed.
+                        */
+                    async function createEventGoogleCallendar (eventToAddToCallendar) {
+
+                        const event = {
+                            'summary': `${eventToAddToCallendar.eventName}`,
+                            'location': `${eventToAddToCallendar.eventBuildingNumber}, ${eventToAddToCallendar.eventStreetName}, ${eventToAddToCallendar.eventCity}, ${eventToAddToCallendar.eventCounty}, ${eventToAddToCallendar.eventCountry}, ${eventToAddToCallendar.eventPostCode}`,
+                            'description': 'A walk in the woods',
+                            'start': {
+                              'dateTime': `${eventToAddToCallendar.eventStartDate}`,
+                              'timeZone': 'Europe/London'
+                            },
+                            'end': {
+                              'dateTime': `${eventToAddToCallendar.eventEndDate}`,
+                              'timeZone': 'Europe/London'
+                            }
+                          };
+                          
+                          const request = gapi.client.calendar.events.insert({
+                            'calendarId': 'primary',
+                            'resource': event
+                          });
+                          
+                          request.execute(function(event) {
+                            //appendPre('Event created: ' + event.htmlLink);
+                            eventLinkButton.setAttribute('href', event.htmlLink)
+                          });
+                    }
             })
         })
-    }
 }
+/*
+
+google code:
+
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
 
 /*
             eventsDisplay = []
