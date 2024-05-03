d2f3f0eaef7b53762e3ce2aec41bf731842c2ffc
Poddyman0 <127142059+Poddyman0@users.noreply.github.com>
2024-05-03 18:20:15 +0100

    google login

--- events.js
+++ events.js
@@ -1,31 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     loadEvents()
 
-    // Fetch API request for gapi.js
-fetch('https://apis.google.com/js/api.js')
-.then(response => response.text())
-.then(scriptText => {
-    // Create script element
-    console.log("gapi", scriptText)
-    gapiLoaded;
-
-    // Append script element to the document's body
 })
-.catch(error => console.error('Error loading gapi.js:', error));
 
-// Fetch API request for GSI client
-fetch('https://accounts.google.com/gsi/client')
-.then(response => response.text())
-.then(scriptText => {
-    // Create script element
-    console.log("gis", scriptText)
-    gisLoaded;
-
-    // Append script element to the document's body
-})
-.catch(error => console.error('Error loading GSI client:', error));
-    gapiLoaded
-})
 /*
 let eventIDToView = ""
 
@@ -53,63 +30,6 @@ let eventsArray = []
 
 //individuals events
 let eventCallendar = []
-
-
-
-    /* exported gapiLoaded */
-  /* exported gisLoaded */
-  /* exported handleAuthClick */
-  /* exported handleSignoutClick */
-
-  // TODO(developer): Set to client ID and API key from the Developer Console
-  const CLIENT_ID = '1089595369803-8aebmjojlqmos49ifvg049io1e4rpeje.apps.googleusercontent.com';
-  const API_KEY = 'AIzaSyAJklJeZ0-lvOU2qvp8Ae-ycgf8vbyp660';
-
-  // Discovery doc URL for APIs used by the quickstart
-  const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
-
-  // Authorization scopes required by the API; multiple scopes can be
-  // included, separated by spaces.
-  const SCOPES = 'https://www.googleapis.com/auth/calendar';
-
-  let tokenClient;
-  let gapiInited = false;
-  let gisInited = false;
-
-  /**
-   * Callback after api.js is loaded.
-   */
-  function gapiLoaded() {
-    gapi.load('client', initializeGapiClient);
-  }
-
-  /**
-   * Callback after the API client is loaded. Loads the
-   * discovery doc to initialize the API.
-   */
-  async function initializeGapiClient() {
-    await gapi.client.init({
-      apiKey: API_KEY,
-      discoveryDocs: [DISCOVERY_DOC],
-    });
-    gapiInited = true;
-    //maybeEnableButtons();
-  }
-
-  /**
-   * Callback after Google Identity Services are loaded.
-   */
-  function gisLoaded() {
-    tokenClient = google.accounts.oauth2.initTokenClient({
-      client_id: CLIENT_ID,
-      scope: SCOPES,
-      callback: '', // defined later
-    });
-    gisInited = true;
-    //maybeEnableButtons();
-  }
-
-
 
 
 
